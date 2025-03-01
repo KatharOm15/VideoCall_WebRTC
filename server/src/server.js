7477a51accde06603763b6c56689787da1962cf7
om kathar <omkathar15@gmail.com>
2025-03-01 23:06:50 +0530

    modified server

--- server/src/server.js
+++ server/src/server.js
@@ -6,6 +6,10 @@ const { networkInterfaces } = require('os');
 
 const app = express();
 app.use(cors());
+
+app.get("/",(req,res)=>{
+  console.log("working")
+})
  
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
